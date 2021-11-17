0974c469eb1c2f2a16a3eee92ce5eac52dbb2196
lizheng9 <lizheng9@vipkid.com.cn>
2021-11-17 17:56:40 +0800

    feat: ndk回顾

--- app/src/main/obj/local/arm64-v8a/objs/HelloNdkbuild/HelloNdkbuild.o.d
+++ app/src/main/obj/local/arm64-v8a/objs/HelloNdkbuild/HelloNdkbuild.o.d
@@ -1,5 +1,5 @@
-E:/studioDemo/ndkdemoCMake/app/src/main/obj/local/arm64-v8a/objs/HelloNdkbuild/HelloNdkbuild.o: \
-  E:/studioDemo/ndkdemoCMake/app/src/main/jni/HelloNdkbuild.c \
-  E:/studioDemo/ndkdemoCMake/app/src/main/jni/com_aoyang_ndkdemo_JNIForNdkbuild.h
+/Users/lizheng/AndroidStudioProjects/ndkbuild_cmake/app/src/main/obj/local/arm64-v8a/objs/HelloNdkbuild/HelloNdkbuild.o: \
+  /Users/lizheng/AndroidStudioProjects/ndkbuild_cmake/app/src/main/jni/HelloNdkbuild.c \
+  /Users/lizheng/AndroidStudioProjects/ndkbuild_cmake/app/src/main/jni/com_aoyang_ndkdemo_JNIForNdkbuild.h
 
-E:/studioDemo/ndkdemoCMake/app/src/main/jni/com_aoyang_ndkdemo_JNIForNdkbuild.h:
+/Users/lizheng/AndroidStudioProjects/ndkbuild_cmake/app/src/main/jni/com_aoyang_ndkdemo_JNIForNdkbuild.h:
